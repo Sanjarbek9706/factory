aca33689b7fcc83771c085718c231befb209f833
Sanjarbek9706 <san19970618@gmail.com>
2026-07-19 23:05:17 +0900

    fix: organize spa & srr business logic

--- src/controllers/factory.controller.ts
+++ src/controllers/factory.controller.ts
@@ -4,6 +4,7 @@ import MemberService from  "../models/Member.service"
 import { LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
+const memberService = new MemberService();
 
 const factoryController: T = {};
 factoryController.goHome = (req: Request, res: Response) => {
@@ -12,15 +13,6 @@ factoryController.goHome = (req: Request, res: Response) => {
       res.send("Home Page");
     } catch (err) {
         console.log("Error, goHome:", err);
-    }
-};
-
-factoryController.getLogin = (req: Request, res: Response) => {
-    try {
-        console.log("getLogin");
-      res.send("Login Page");
-    } catch (err) {
-        console.log("Error, getLogin:", err);
     }
 };
 
@@ -33,34 +25,22 @@ factoryController.getSignup = (req: Request, res: Response) => {
     }
 };
 
-factoryController.processLogin = async (req: Request, res: Response) => {
+factoryController.getLogin = (req: Request, res: Response) => {
     try {
-        console.log("processLogin");
-        console.log("body", req.body);
-        const input: LoginInput = req.body;
-
-        const memberService = new MemberService();
-        const result = await memberService.processLogin(input);
-
-
-        res.send(result);
+        console.log("getLogin");
+      res.send("Login Page");
     } catch (err) {
-        console.log("Error, processLogin:", err);
-        res.send(err);
+        console.log("Error, getLogin:", err);
     }
 };
+
 
 factoryController.processSignup = async (req: Request, res: Response) => {
     try {
         console.log("processSignup")
-        console.log("body:", req.body);
-
 
         const newMember: MemberInput = req.body;
         newMember.memberType = MemberType.FACTORY;
-
-
-        const memberService = new MemberService();
         const result = await memberService.processSignup(newMember);
 
         res.send(result);
@@ -69,5 +49,21 @@ factoryController.processSignup = async (req: Request, res: Response) => {
         res.send(err);
     }
 };
+
+factoryController.processLogin = async (req: Request, res: Response) => {
+    try {
+        console.log("processLogin");
+
+        const input: LoginInput = req.body;
+        const result = await memberService.processLogin(input);
+
+        res.send(result);
+    } catch (err) {
+        console.log("Error, processLogin:", err);
+        res.send(err);
+    }
+};
+
+
 
 export default factoryController;
