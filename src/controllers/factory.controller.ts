5b43f74451aa140d894a2ea5939eccb3b8a3d226
Sanjarbek9706 <san19970618@gmail.com>
2026-08-03 12:11:35 +0900

    fix: modify restaurant controller  processSignup and processLogin

--- src/controllers/factory.controller.ts
+++ src/controllers/factory.controller.ts
@@ -3,7 +3,7 @@ import { T } from "../libs/types/common";
 import MemberService from  "../models/Member.service"
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
-import Errors, { Message } from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 
 const memberService = new MemberService();
 
@@ -42,14 +42,18 @@ factoryController.getLogin = (req: Request, res: Response) => {
 factoryController.processSignup = async (req: AdminRequest, res: Response) => {
     try {
         console.log("processSignup")
+        const file = req.file;
+        if(!file) throw new Errors(HttpCode.BAD_REQUEST, Message.SOMETHING_WENT_WRONG);
+        
 
         const newMember: MemberInput = req.body;
+        newMember.memberImage = file?.path;
         newMember.memberType = MemberType.FACTORY;
         const result = await memberService.processSignup(newMember);
 
         req.session.member  = result;
         req.session.save(function() {
-            res.send(result);
+            res.redirect("/admin/product/all");
         }); 
     } catch (err) {
         console.log("Error, processSignup:", err);
@@ -67,7 +71,7 @@ factoryController.processLogin = async (req: AdminRequest, res: Response) => {
 
         req.session.member  = result;
         req.session.save(function() {
-            res.send(result);
+            res.redirect("/admin/product/all");
         }); 
 
     } catch (err) {
